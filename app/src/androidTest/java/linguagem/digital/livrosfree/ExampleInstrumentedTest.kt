245865d812d7db3a0061905168343f2edf536f35
Gabriel Alencar <gabriel@alencar.site>
2020-10-12 01:26:47 -0300

    Start

---
+++ app/src/androidTest/java/linguagem/digital/livrosfree/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package linguagem.digital.livrosfree
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("linguagem.digital.livrosfree", appContext.packageName)
+    }
+}
